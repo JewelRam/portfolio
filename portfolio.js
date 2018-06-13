3bc61df7c374f11d201230cf36904bff568d697e
Jewel Ramirez <chimpy4u@yahoo.com>
2018-06-13 08:52:45 -0700

    i dont remember what i changed

--- portfolio.js
+++ portfolio.js
@@ -1,58 +1,73 @@
-let portfolio = Object.create(Object.prototype, {
-  company: { writable: true, enumerable: true, value: "Poopy's"
-  },
-  specialty: {writable: true, enumerable: true, value: "poop"
-  },
-   name: {enumerable: true, value: "daddy"},
-   portfolio: {enumerable: false, 
-    value:[{
-        name: "SRC",
-        shares: 12,
-        valuation: 1200,
-    }],
-  
-},  worth: {
-    enumerable: false,
-   value: function () {
-      let netWorth = this.portfolio;
-      let dollarAmount = 0;
-      netWorth.forEach(object => {
-          dollarAmount += object.valuation
-      })
-         
-      return dollarAmount  
+let portfolio = Object.create({
+    company: {
+        writable: true, enumerable: true, value: "Company Name"
+    },
+    specialty: {
+        writable: true, enumerable: true, value: "The Specialty"
+    },
+    name: { enumerable: true, value: "Name" },
+    portfolio: {
+        enumerable: false,
+        value: [{
+            name: "SRC",
+            shares: 12,
+            valuation: 1200,
+        }],
 
-}
-},
-    purchase: {
-    enumerable: false,
-    value:  function (ticker, shareAmount, price) {
-      portfolio.portfolio.push( 
-        {
-            name: ticker,
-            shares: shareAmount,
-            valuation: price,
-            sold: false,
+    }, worth: {
+        enumerable: false,
+        value: function () {
+            let netWorth = this.portfolio;
+            let dollarAmount = 0;
+            netWorth.forEach(object => {
+                dollarAmount += object.valuation
+            })
+
+            return dollarAmount
+
         }
-      )
- },
+    },
+    purchase: {
+        enumerable: false,
+        value: function (ticker, shareAmount, price) {
+            this.portfolio.push(
+                {
+                    name: ticker,
+                    shares: shareAmount,
+                    valuation: price,
+                    sold: true,
+                }
+            )
+        },
     },
     sell: {
         enumerable: false,
-       value: function (ticker, shareAmount, price) {
-         portfolio.portfolio.push(
-             {
-                 name: ticker,
-                 shares: shareAmount,
-                 valuation: price,
-                 sold: true
-             }
-         )
+        value: function (ticker, shareAmount, price) {
+            this.portfolio.push(
+                {
+                    name: ticker,
+                    shares: shareAmount,
+                    valuation: price,
+                    sold: false
+                }
+            )
         }
-    }  
+    }
 
 
 })
 console.log(portfolio.sell("POOP", 4, 3400))
 console.log(portfolio.portfolio)
 console.log(portfolio.worth())
+
+// challenge
+const fragment = document.createDocumentFragment()
+const articles = document.querySelector('#art')
+
+for (let key in portfolio) {
+
+    const section = document.createElement('section')
+    section.textContent = `${key.charAt(0).toLocaleUpperCase() + key.slice(1)}: ${portfolio[key]}`
+
+    fragment.appendChild(section)
+}
